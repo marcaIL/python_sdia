1f69b8f43e1800d250feafa66f05b9b3793d67d8
lsol1 <a>
2025-09-29 17:33:21 +0200

    chore: doc

--- Labs/Lab3/functions_ex_1.py
+++ Labs/Lab3/functions_ex_1.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def interpolate(A,B):
+    """
+    Compute the interpolation intersection between the two vectors and unit ball
+    """
     norm_A = np.linalg.norm(A)
     norm_B = np.linalg.norm(B)
+    #We check the existence of an intersection with unit ball
     simple_check(A,B,norm_A,norm_B)
+    #After calculation, we want to find the root of this polynom
+    #between 0 and 1 (should exist now)
     coeffs = np.array([
        np.linalg.norm(A-B)**2,
        -2*norm_B**2 + 2*np.dot(A,B),
@@ -18,6 +24,19 @@ def interpolate(A,B):
 
 
 def simple_check(A,B,norm_A,norm_B):
+    """
+    Check quickly the existence of an intersection with the unit ball
+    and return the smallest vector
+
+    Args:
+    A(np.ndarray): first vector
+    B(np.ndarray): second vector
+    norm_A(double): norm of first vector
+    norm_B(double): norm of second vector
+
+    Return:
+    np.ndarray: None or the smallest vector
+    """
     if (norm_A<1 and norm_B<1):
         return None
     elif (norm_A==1  and norm_B<1):
@@ -29,7 +48,17 @@ def simple_check(A,B,norm_A,norm_B):
 
 
 def plot_brownian_motion(nb,N,x,step,rng,brownian_f):
-    colors=["red","pink","yellow","green","orange"]
+
+    """
+    Plot a fixed number of brownian trajectories and crossing point with the unit ball
+
+    Args:
+    nb(int): number of trajectories between 0 and 5
+    brownian_f(function): brownian movement function
+    """
+    if (nb<0 or nb>5):
+        return
+    colors=["red","pink","yellow","green","orange","blue"]
     circle = plt.Circle((0,0), 1)
     fig, ax = plt.subplots()
     plt.xlim(-1.25,1.25)
--- Labs/Lab3/functions_ex_2.py
+++ Labs/Lab3/functions_ex_2.py
@@ -3,6 +3,18 @@ from scipy.signal.windows import gaussian
 
 
 def generate_gaussian_kernel(sigma_X,sigma_Y,M,N):
+    """
+    Generates a gaussian window of given size with given standard deviations.
+
+    Args:
+    sigma_X(double): x standard deviation
+    sigma_Y(double): y standard deviation
+    M(int): first dimension of the window
+    N(int): second dimension of the window
+
+    Return:
+    np.ndarray: gaussian kernel of size MxN
+    """
     w_x=gaussian(M,std=sigma_X)
     w_y=gaussian(N,std=sigma_Y)
     kernel=w_y[:,np.newaxis]*w_x[np.newaxis,:]
@@ -10,6 +22,9 @@ def generate_gaussian_kernel(sigma_X,sigma_Y,M,N):
     return kernel
 
 def padding(X,M,N):
+    """
+    Computes the padding of a given matrix X, with the required dimensions MxN
+    """
     shp=X.shape
     if (shp[0]>M or shp[1]>N):
         raise Exception("Dimensions problem")
